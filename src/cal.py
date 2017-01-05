f1571f0725eefb7e9a0a72316785b09f0bf82380
Vaibhav Bhasin <bhasinvaibhav97@gmail.com>
2017-01-06 00:46:38 +0530

    Convert cal.py code into a class

--- src/cal.py
+++ src/cal.py
@@ -16,66 +16,68 @@ except ImportError:
     flags = None
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
-CLIENT_SECRET_FILE = 'client_secret.json'
+#CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Shoutboxx Calendar'
 
+class GoogleCalendar():
+    def __init__(self,client_secret):
+        #Make sure to pass client_secret as a string with path to the json file.
+        self.client_secret=client_secret
 
-def get_credentials():
-    """Gets valid user credentials from storage.
+    def get_credentials():
+        """Gets valid user credentials from storage.
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'calendar-python-quickstart.json')
 
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(self.client_secret, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else: # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
 
-def main():
-    
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
+    def newEvent(event):
 
-    event = {
-    'summary': 'Test Event',
-    'location': 'BITS Pilani, Hyderabad Campus',
-    'description': 'Insert random stuff here',
-    'start': {
-        'dateTime': '2017-01-04T01:35:00+05:30',
-        'timeZone': 'Asia/Kolkata',
-    },
-    'end': {
-        'dateTime': '2017-01-04T02:35:00+05:30',
-        'timeZone': 'Asia/Kolkata',
-    },
-    'reminders': {
-        'useDefault': False,
-        'overrides': [
-        {'method': 'email', 'minutes': 1},
-        {'method': 'popup', 'minutes': 1},
-      ],
-    },
-  }
+        credentials = get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
 
-    event = service.events().insert(calendarId='primary', body=event).execute()
+        #Format for event object:
+        """event = {
+        'summary': 'Test Event',
+        'location': 'BITS Pilani, Hyderabad Campus',
+        'description': 'Insert random stuff here',
+        'start': {
+            'dateTime': '2017-01-04T01:35:00+05:30',
+            'timeZone': 'Asia/Kolkata',
+        },
+        'end': {
+            'dateTime': '2017-01-04T02:35:00+05:30',
+            'timeZone': 'Asia/Kolkata',
+        },
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+            {'method': 'email', 'minutes': 1},
+            {'method': 'popup', 'minutes': 1},
+          ],
+        },
+      }"""
 
-if __name__ == '__main__':
-    main()
+        event = service.events().insert(calendarId='primary', body=event).execute()
