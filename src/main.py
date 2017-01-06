30f133ac5ee09512c95ddc15718eeff94d135832
Rohitt Vashishtha <aero31aero@gmail.com>
2017-01-06 19:00:22 +0530

    Add Event Detection to main code.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,29 @@
 #! /usr/bin/env python3
 import post
 import database
+import event
 
 import os
+
 def run():
 	apitoken = os.environ['FACEPY_TOKEN']
 	groupid = '300189226756572'
 	db = database.DatabaseHandler("localhost","root","root","sbxcal")
 	db.setupDatabase()
+	evt = event.EventScraper()
 	facebook = post.FacebookHandler(apitoken,groupid)
 	allposts = facebook.scrape(db.getLatestPostTime())
 	print(len(allposts))
 	for eachpost in allposts:
 		try:
 			db.insertPost(eachpost)
+			if evt.isEvent(eachpost):
+				newevent = evt.getEvent(eachpost)
+				db.insertEvent(newevent)
+				print(newevent)
 		except KeyError as e:
+			# swallow the exception
 			print(type(e))
 			print(e)
+			# pass
 run()
